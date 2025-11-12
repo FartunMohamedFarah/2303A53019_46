37e29fa9354d90e0eb7a52398d54a73364114ae7
FartunMohamedFarah <167029094+FartunMohamedFarah@users.noreply.github.com>
2025-11-12 14:31:48 +0530

    Delete Weather directory

--- Weather/config.example.mjs
+++
@@ -1,4 +0,0 @@
-// Copy this file to config.mjs and put your OpenWeatherMap API key there.
-// Add config.mjs to .gitignore so you don't commit your secret key.
-
-export const API_KEY = 'YOUR_OPENWEATHERMAP_API_KEY_HERE';
